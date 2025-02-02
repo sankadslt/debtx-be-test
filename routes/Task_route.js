57c2820f61f02de55bd3468be0e2e90ea8f27e6a
debtx <sankad@slt.com.lk>
2025-02-02 22:01:46 +0530

    delete files

--- routes/Task_route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { createTask } from '../services/TaskService.js';
-
-const router = express.Router();
-
-router.post('/Create_Task', createTask);
-
-export default router;
